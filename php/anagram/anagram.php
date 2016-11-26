45b49626b879eeffa1803db833e6151e7f8c605f
jeslopcru <jeslopcru@gmail.com>
2016-11-26 18:22:29 +0100

    algorithm

--- php/anagram/anagram.php
+++ php/anagram/anagram.php
@@ -1,10 +1,27 @@
 <?php
 
-function detectAnagrams($word)
+function detectAnagrams($word, array $possibles)
 {
     $anagrams = [];
-    if ($word == 'ant') {
-        $anagrams[] = 'tan';
+
+    foreach ($possibles as $possible) {
+        $anagramLetters = str_split($word);
+        $possibleLetters = str_split($possible);
+
+        if (count($anagramLetters) === count($possibleLetters)) {
+            foreach ($possibleLetters as $character) {
+                $index = array_search($character, $anagramLetters);
+
+                if ($index !== false) {
+                    unset($anagramLetters[$index]);
+                }
+
+                if (empty($anagramLetters)) {
+                    $anagrams[] = $possible;
+                }
+            }
+        }
     }
+
     return $anagrams;
 }
