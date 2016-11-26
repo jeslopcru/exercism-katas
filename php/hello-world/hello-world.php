1fbfef29aea82572deb7ea1f4762b96c53576420
jeslopcru <jeslopcru@gmail.com>
2016-11-26 12:19:43 +0100

    my solution

--- php/hello-world/hello-world.php
+++ php/hello-world/hello-world.php
@@ -1,13 +1,10 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hello World" exercise.
-// It's been provided as a convenience to get you started writing code faster.
-//
-
 function helloWorld($name = null)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+    if (is_null($name)) {
+        $name = 'World';
+    }
+
+    return "Hello, {$name}!";
 }
